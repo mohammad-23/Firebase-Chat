6f758f18564a59afc2d51487d489f2b0c77ed54d
Mohammad <mohammad8758@hotmail.com>
2019-03-12 14:46:42 +0530

    Fixed Config error

---
+++ src/config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyDbrW1PpnbvfAfMlATbajl1dRvwm2jjA8Y",
+    authDomain: "chat-firebase-0001.firebaseapp.com",
+    databaseURL: "https://chat-firebase-0001.firebaseio.com",
+    projectId: "chat-firebase-0001",
+    storageBucket: "chat-firebase-0001.appspot.com",
+    messagingSenderId: "369170871620"
+}
